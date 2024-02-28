8ffb97bc30f12d09efb39cb76b3b4942a9508879
thaybrrt <thaybarreto@live.com>
2024-02-28 11:16:12 -0300

    Home-navbar-footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+function Navbar() {
+ 
+  
+
+  return (
+    <>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <div className='text-2xl font-bold uppercase'>Farmácia</div>
+
+            <div className='flex gap-4'>
+              <div className='hover:underline'>Produtos</div>
+              <div className='hover:underline'>Categorias</div>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
