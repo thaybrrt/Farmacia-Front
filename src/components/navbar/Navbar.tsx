6306a6829f721488bc22dbf9a8a84571672c41e4
thaybrrt <thaybarreto@live.com>
2024-02-28 23:56:14 -0300

    rotas correção conclusao-do-projeto

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar() {
             <Link to='/home' className='text-2xl font-bold uppercase'>Farmácia Quero Remédios</Link>
 
             <div className='flex gap-3 cursor-pointer'>
-                        <Link to='/home' className='hover:bg-[#489c79] py-1 px-4 rounded-3xl duration-300'>Home</Link>
+                        <Link to='/home' className='hover:bg-[#489c79] py-1 px-4 rounded-3xl duration-300'>Iniciar</Link>
                         <Link to='/categorias' className='hover:bg-[#489c79] py-1 px-4 rounded-3xl duration-300'>Categorias</Link>
                         <Link to='/cadastroCategoria' className='hover:bg-[#489c79] py-1 px-4 rounded-3xl duration-300'>Cadastrar Categoria</Link>
                     </div>
